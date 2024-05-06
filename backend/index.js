0ed330ace1f559eddfa3c1c6fc70269b0077ffb9
mihir <mihirjetpariya2003@gmail.com>
2024-05-06 18:43:17 +0530

    test api is created

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose, { mongo } from "mongoose";
 import dotenv from 'dotenv';
+import userRouter from "./routes/user.route.js";
 
 const app = express();
 
@@ -14,8 +15,9 @@ mongoose.connect(process.env.MONGO).then(()=> {
 
 const port = 3000;
 
+app.use(express.json());
 
-
+app.use('/api/user' , userRouter);
 
 
 app.listen(port, ()=> {
---
+++ backend/routes/user.route.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { test } from '../Controllers/user.controller.js';
+
+
+
+
+const router = express.Router();
+
+
+router.get('/temp' , test);
+
+
+export default router;
